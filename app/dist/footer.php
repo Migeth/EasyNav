e5fbc9ba98004724e26f56adc6099580b9a46601
Migeth <sethmigandassi@gmail.com>
2022-06-24 01:10:43 +0200

    ajout du dossier App dans lequel il y a la partie de l'application principale

---
+++ app/dist/footer.php
@@ -0,0 +1,10 @@
+<?php ?>
+
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2022 <div class="bullet"></div> Design By <a href="#">4 IADB Students</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+</footer>
