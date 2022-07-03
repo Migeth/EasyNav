4eed14580c20f65d5d6c08014c2edc8545e092df
Sarahnait1999 <47569271+Sarahnait1999@users.noreply.github.com>
2022-07-03 19:04:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req,res) =>{
   res.statusCode =200;
   res.setHeader('Content-Type' ,'text/plain');
-  res.end(include(sign-in.php));
+  res.end('helloworls');
 
 server.listen(port, () => {
   console.log('Server running at http://localhost:${port}:');
