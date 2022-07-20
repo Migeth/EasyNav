196a6d31414d5a8cac89f73ad8978a8c9a16fa5d
EC2 Default User <ec2-user@ip-172-31-85-244.ec2.internal>
2022-07-20 20:26:40 +0000

    connexion base de donnée

---
+++ dbinfo.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_SERVER', 'php-first-rds.cfea1hbhzrza.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'sarahNH25');
+define('DB_DATABASE', 'users');
+
+?>
+                
