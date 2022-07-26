3914be8f1ae6697f6fd9d889813b1813e32c5904
Sarahnait1999 <47569271+Sarahnait1999@users.noreply.github.com>
2022-07-26 16:06:16 +0200

    Update dbinfo.inc.php

--- dbinfo.inc.php
+++ dbinfo.inc.php
@@ -1,9 +1,47 @@
-<?php
+   <?php
+            $servername = 'database-rds-php.cfea1hbhzrza.us-east-1.rds.amazonaws.com';
+            $username = 'root';
+            $password = 'sarahNH25';
+            $basedonn= 'users';
+            //On établit la connexio       
+            //
 
-define('DB_SERVER', 'php-first-rds.cfea1hbhzrza.us-east-1.rds.amazonaws.com');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'sarahNH25');
-define('DB_DATABASE', 'users');
+            $db = new PDO("mysql:host=$servername;dbname=$basedonn", $username, $password);
 
-?>
-                
+try{
+
+
+            //$sql = "CREATE DATABASE historique";
+           //$db->exec($sql);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  //      echo 'Base de données créée bien créée !';
+//        echo "Connexion réussi ";
+
+
+
+//var_dump($recipes);
+
+}catch (PDOException $e){
+        die("Erreur:".$e->getMessage());
+}
+
+
+//ici on est connecté a la base 
+
+
+//$sql ="INSERT INTO `USERS`(`NamePrenom`, `Email`, `password`) VALUES ('ninanait','nina@gmail.com','dfvdfrvbr')";
+      
+    
+//$requete = $db->query($sql);
+ 
+      
+//On recupere les donnee
+//$user = $requete->fetchAll(); 
+      
+
+//var_dump($user);
+//$requete = execute();
+    
+?>  
+
+    
